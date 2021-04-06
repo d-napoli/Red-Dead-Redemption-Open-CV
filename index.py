9bec32d13690331ace249bd153d6b48a80dc5205
Diego Napoli <37269543+d-napoli@users.noreply.github.com>
2021-04-06 18:02:23 -0300

    Update index.py

--- index.py
+++ index.py
@@ -7,50 +7,48 @@ import keys as k
 keys = k.Keys()
 
 def pathing(minimap):
-    lower = np.array([100, 130, 100])
-    upper = np.array([150, 255, 255])
+    limite_inferior = np.array([100, 130, 100])
+    limite_superior = np.array([150, 255, 255])
 
     hsv = cv2.cvtColor(minimap, cv2.COLOR_RGB2HSV)
-    mask = cv2.inRange(hsv, lower, upper)
+    mascara = cv2.inRange(hsv, limite_inferior, limite_superior)
 
-    matches = np.argwhere(mask==255) # array of matches pixels that are white on the mini map
+    matches = np.argwhere(mascara == 255) # array com os índices que batem o filtro da máscara
     
-    mean_y = np.mean(matches[:,1]) # indexes of the Y values that match the white pixels
-    target = minimap.shape[1] / 2 # the center of the screen
+    caminho_encontrado = np.mean(matches[:,1]) # pega os valores do índice Y da variável matches
+    centro_tela = minimap.shape[1] / 2 # pega o centro da tela, de acordo com o tamanho do mini mapa
 
-    error = target - mean_y # the difference between the white values and the target value
-    print(error)
+    diferenca_centro = centro_tela - caminho_encontrado # a diferença do caminho atual até o centro da tela
+    print(diferenca_centro)
 
-    keys.directMouse(-1 * int(error * 1.7), 0)
+    keys.directMouse(-1 * int(diferenca_centro * 1.6), 0)
 
-    cv2.imshow("cv2screen", mask)
+    cv2.imshow("cv2screen", mascara)
     cv2.waitKey(5)
 
-for i in range(5): # just some time until things start running
+for i in range(5): # timeout antes de a execução começar
     print(i)
     time.sleep(1)
 
 keys.directKey("w")
 
 for i in range(40000):
-    img = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
-    img_np = np.array(img)
-    screen = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+    image_frame = ImageGrab.grab(bbox=(0, 0, 1920, 1080))
+    image_np = np.array(image_frame)
+    tela = cv2.cvtColor(image_np, cv2.COLOR_BGR2RGB)
 
-    # we wanna grab the region of the minimap on the screen
-    minimap = screen[650:900, 60:310]
-    miniminimap = screen[690:780, 140:230]
+    miniminimap = tela[710:780, 140:230]
 
     pathing(miniminimap)
 
     if(i % 10 == 0):
         keys.directKey("LSHIFT")
 
-    if(i % 12 == 0):
+    if(i % 11 == 0):
         keys.directKey("LSHIFT", keys.key_release)
 
-    # screen = cv2.resize(screen, (960, 540))
-    # cv2.imshow("cv2screen", miniminimap)
+    # tela = cv2.resize(tela, (960, 540))
+    # cv2.imshow("cv2tela", miniminimap)
     # cv2.waitKey(5)
 keys.directKey("w", keys.key_release)
 cv2.destroyAllWindows()
